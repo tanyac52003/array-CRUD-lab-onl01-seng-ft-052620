db74242389da657c4688d3ff0be62325a4e98494
Tanya Hodges <tanyac52003@gmail.com>
2020-05-27 20:12:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop(array[4])
+  array[4].pop
 end
 
 def remove_element_from_start_of_array(array)
